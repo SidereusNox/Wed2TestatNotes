494c966c80aba0223938ef2556ec39e721421854
MasterR8 <philbo96@gmail.com>
2019-10-28 10:50:16 +0100

    Done stuff

--- NotesPro/utils/handlebarHelpers.js
+++ NotesPro/utils/handlebarHelpers.js
@@ -2,4 +2,10 @@ module.exports.registerHelpers = function (handlebars){
     handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
         return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
     });
+    handlebars.registerHelper('times', function(n, block) {
+        var accum = '';
+        for(var i = 0; i < n; ++i)
+            accum += block.fn(i);
+        return accum;
+    });
 }
